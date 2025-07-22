269651c2441382f1022335080c666388dec4cf3f
RandimaD25 <randimamethminid525@gmail.com>
2025-07-22 20:32:58 +0530

    feat: create todo models

--- backend/models/todoModel.js
+++ backend/models/todoModel.js
@@ -1,13 +1,13 @@
 import db from "../config/database.js";
 
 export async function getAllTodos() {
-  const [rows] = await db.query("SELECT * FROM todos");
+  const [rows] = await db.query("SELECT * FROM task");
   return rows;
 }
 
 export async function addTodo(title, description) {
   const [result] = await db.query(
-    "INSERT INTO todos (title, description) VALUES (?, ?)",
+    "INSERT INTO task (title, description) VALUES (?, ?)",
     [title, description]
   );
   return { id: result.insertId, title, description };
